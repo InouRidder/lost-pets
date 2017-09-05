07b7dbb4c7f6588759937a0a10c7835f6082b121
Inou Ridder <inouridder@gmail.com>
2017-09-05 18:29:06 +0200

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Emptying Database"
+
+Pet.destroy_all
+
+puts "Seeding!"
+
+categories = %w(dog cat snake mouse)
+
+15.times do
+  pet = Pet.new(name: Faker::Cat.name, description: Faker::ChuckNorris.fact, category: categories.sample, found_on: Date.today)
+  puts "Created #{pet.name}" if pet.save
+end
