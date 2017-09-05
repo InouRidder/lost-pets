6406912e2a95bae2dbed03b6029394a785ddaa24
Inou Ridder <inouridder@gmail.com>
2017-09-05 18:12:35 +0200

    create controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-
-  get 'pets/show'
-
-  get 'pets/new'
-
-  get 'pets/edit'
-
+  resources :pets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
